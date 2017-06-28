92f35eaca9c08a9e032a75d5cf90d3959f39aeaa
Dennis Plosceanu <dennisplosceanu@gmail.com>
2017-06-28 16:42:49 +0300

    multiboot2 info pointer is now sent to rust_main\npanic function prints a message

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,42 @@ mod vga_buffer;
 extern crate volatile;
 extern crate rlibc;
 extern crate spin;
+extern crate multiboot2;
 
 #[no_mangle]
-pub extern fn rust_main() {
+pub extern fn rust_main(multiboot_info_addr: usize) {
+    let boot_info = unsafe { multiboot2::load(multiboot_info_addr) };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
 
     vga_buffer::clear_screen();
-    println!("{}", { println!("inner"); "outer" });
+    println!("Memory areas:");
+
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}",
+                area.base_addr, area.length);
+    }
+
+    let elf_sections_tag = boot_info.elf_sections_tag()
+        .expect("Elf-sections tag required");
+
+    println!("Kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+                section.addr, section.size, section.flags);
+    }
 
     loop{}
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+#[lang = "eh_personality"]
+extern fn eh_personality() {}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}
 
